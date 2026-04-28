66bd158e4d90ce1cf5e8205889c2ea8e7232152e
shafayethossai <khansh921@gmail.com>
2026-04-29 00:41:52 +0600

    updated

--- backend/rest/middlewares/cors.go
+++ backend/rest/middlewares/cors.go
@@ -5,11 +5,14 @@ import "net/http"
 func Cors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		w.Header().Set("Content-Type", "application/json")
-		w.Header().Set("Cross-Origin-Opener-Policy", "same-origin-allow-popups")
-		w.Header().Set("Cross-Origin-Embedder-Policy", "require-corp")
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
 
 		next.ServeHTTP(w, r)
 	})
--- backend/rest/middlewares/preflight.go
+++ backend/rest/middlewares/preflight.go
@@ -5,10 +5,6 @@ import "net/http"
 func Preflight(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == "OPTIONS" {
-			w.Header().Set("Access-Control-Allow-Origin", "*")
-			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
-			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-			w.Header().Set("Access-Control-Max-Age", "86400")
 			w.WriteHeader(http.StatusOK)
 			return
 		}
