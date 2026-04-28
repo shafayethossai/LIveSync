cf04a6cca9d7b69b69263e10a5c7eea8cb2c1b44
shafayethossai <khansh921@gmail.com>
2026-04-29 00:45:52 +0600

    up

--- backend/rest/middlewares/cors.go
+++ backend/rest/middlewares/cors.go
@@ -5,15 +5,11 @@ import "net/http"
 func Cors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		w.Header().Set("Content-Type", "application/json")
-
-		if r.Method == "OPTIONS" {
-			w.WriteHeader(http.StatusOK)
-			return
-		}
 
 		next.ServeHTTP(w, r)
 	})
 }
+
--- backend/rest/middlewares/preflight.go
+++ backend/rest/middlewares/preflight.go
@@ -5,8 +5,8 @@ import "net/http"
 func Preflight(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == "OPTIONS" {
-			w.WriteHeader(http.StatusOK)
-			return
+			w.WriteHeader(200)
+			return 
 		}
 
 		next.ServeHTTP(w, r)
